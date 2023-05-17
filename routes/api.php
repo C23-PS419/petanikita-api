c3c504fdddd62f603b18411332d402a8faddafb7
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-18 06:48:40 +0700

    Move Tokenization method from routes/api file to TokenController

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,6 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Api\User\TokenController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        return response()->json([
-            'message' => 'The provided credentials are incorrect.',
-        ], 401);
-    }
-
-    return response()->json([
-        'token' => $user->createToken($request->device_name)->plainTextToken,
-        'user' => $user,
-    ]);
-});
+Route::post('/token', TokenController::class);
