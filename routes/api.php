b3caa8a25114310dde9564d1f56c7c46521dfa9a
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-18 06:34:41 +0700

    Create user token request API

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,27 @@
 <?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json([
+            'message' => 'The provided credentials are incorrect.',
+        ], 401);
+    }
+
+    return response()->json([
+        'token' => $user->createToken($request->device_name)->plainTextToken,
+        'user' => $user,
+    ]);
+});
