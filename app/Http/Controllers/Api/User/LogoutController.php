f778ed00077316132f0502fc940d698d9c51db86
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-18 23:01:14 +0700

    Create user logout revoke token endpoint

---
+++ app/Http/Controllers/Api/User/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+
+class LogoutController extends Controller
+{
+    public function __invoke()
+    {
+        $token = request()->user()->currentAccessToken();
+
+        $token->delete();
+
+        return response()->json([
+            'message' => 'Token revoked',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\User\LogoutController;
 use App\Http\Controllers\Api\User\TokenController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/token', TokenController::class);
+
+// TODO: Route register
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user/logout', LogoutController::class);
+
+    // TODO: Route product
+});
