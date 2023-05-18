af369fd27d7d2ee2b395b88a760a38aaf72b5e67
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-18 23:19:15 +0700

    Create register api

---
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        if (User::where('email', $request->email)->first()) {
+            return response()->json([
+                'message' => 'Email already registered',
+            ], 409);
+        }
+
+        $user = User::create($request->only([
+            'name',
+            'email',
+            'password',
+        ]));
+
+        return response()->json([
+            'message' => 'Register success',
+            'data' => $user,
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\User\LogoutController;
+use App\Http\Controllers\Api\User\RegisterController;
 use App\Http\Controllers\Api\User\TokenController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/token', TokenController::class);
-
-// TODO: Route register
+Route::post('/user/register', RegisterController::class);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user/logout', LogoutController::class);
