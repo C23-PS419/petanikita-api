f64c2008d07ca4c3d06fb80706743f1a410e15e7
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-19 04:48:59 +0700

    Fix register API exception handling

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -17,17 +18,22 @@ class RegisterController extends Controller
         ]);
 
         try {
+
             $user = User::create($request->only([
                 'name',
                 'email',
                 'password',
             ]));
+
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => str($e->errorInfo[2])
+                    ->after('DETAIL:  '),
+            ], 409);
         } catch (\Exception $e) {
             return response()->json([
-                'message' => str($e->getMessage())
-                    ->after('DETAIL:  ')
-                    ->before('. (Connection'),
-            ], 409);
+                'message' => $e->getMessage(),
+            ], 500);
         }
 
         return response()->json([
