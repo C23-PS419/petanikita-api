c14277ee0d9be8fbf43d1fba619a6599c7f477ee
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-19 20:57:55 +0700

    Product and User models relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $guarded = [
         'id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
