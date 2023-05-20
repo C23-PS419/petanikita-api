ea85fbf7434ebc307a691dcb8da878015ce2926a
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-21 04:09:26 +0700

    code cleanup

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Product extends Model
 {
     use HasFactory;
+
     protected $guarded = [
         'id',
     ];
