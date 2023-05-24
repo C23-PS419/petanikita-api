95b9b3bc2788864429e76b4ad4e483ed71e4408c
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-24 19:28:23 +0700

    Add phone column to user table

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'phone' => $this->phone,
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
