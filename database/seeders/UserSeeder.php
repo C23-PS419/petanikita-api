a4aa3d99ac439dbc71de542136572e2ea4fe3aea
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-24 19:56:19 +0700

    update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,16 +13,19 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@petanikita.com',
+            'phone' => '08383838383',
             'password' => Hash::make('admin123'),
         ]);
         User::factory()->create([
             'name' => 'Risang Baskoro',
             'email' => 'risangbaskoro@icloud.com',
+            'phone' => '08383838381',
             'password' => Hash::make('password'),
         ]);
         User::factory()->create([
             'name' => 'Irsyad Abqori',
             'email' => 'aleirsyad46@gmail.com',
+            'phone' => '08383838382',
             'password' => Hash::make('password'),
         ]);
     }
