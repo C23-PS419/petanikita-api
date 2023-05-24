10054bd967aa244f24a148d45a88804fd1fb47c8
Risang Baskoro <risangbaskoro@gmail.com>
2023-05-24 19:56:34 +0700

    fix RegisterController logic

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -14,30 +12,17 @@ class RegisterController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255'],
-            'phone' => ['required', 'string', 'min:10', 'max:13'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'phone' => ['required', 'string', 'min:10', 'max:13', 'unique:users,phone'],
             'password' => ['required', 'string', 'min:8'],
         ]);
 
-        try {
-            User::create($request->only([
-                'name',
-                'email',
-                'phone',
-                'password',
-            ]));
-
-        } catch (QueryException $e) {
-            return response()->json([
-                'message' => Str::of($e->errorInfo[2])
-                    ->before("\n")
-                    ->trim(),
-            ], 409);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => $e->getMessage(),
-            ], 500);
-        }
+        User::create($request->only([
+            'name',
+            'email',
+            'phone',
+            'password',
+        ]));
 
         return response(null, 201);
     }
