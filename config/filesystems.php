6aad64cfde4c222fe737699af8bb11475bed3e58
dimas-ishaq <masjamad2@gmail.com>
2023-06-14 10:51:09 +0700

    update deploy-gcr-docker.yml

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,7 +57,7 @@ return [
         ],
         'gcs' => [
             'driver' => 'gcs',
-            'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', null), // optional: /path/to/service-account.json
+            'key_file_path' => env('GCP_CREDENTIALS', null), // optional: /path/to/service-account.json
             'key_file' => [], // optional: Array of data that substitutes the .json file (see below)
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'), // optional: is included in key file
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
